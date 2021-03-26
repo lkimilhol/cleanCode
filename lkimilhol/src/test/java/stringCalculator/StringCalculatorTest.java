7a4278d1c59dbbb2ab28e9c65085ef81a5d0c764
lkimilhol <lkimilhol@gmail.com>
2021-03-26 17:46:14 +0900

    계산기 작성

--- lkimilhol/src/test/java/stringCalculator/StringCalculatorTest.java
+++ lkimilhol/src/test/java/stringCalculator/StringCalculatorTest.java
@@ -16,6 +16,7 @@ class StringCalculatorTest {
     }
 
     @Test
+    @DisplayName("문자열에서 숫자 추출")
     void injectNumbers() {
         StringCalculator stringCalculator = new StringCalculator("1+2");
         String operator = stringCalculator.injectOperator();
@@ -26,8 +27,34 @@ class StringCalculatorTest {
     }
 
     @Test
-    void plus() {
-        StringCalculator stringCalculator = new StringCalculator("1+1");
+    @DisplayName("+ 계산하기")
+    void calcPlus() {
+        StringCalculator stringCalculator = new StringCalculator("1+2");
+        int result = stringCalculator.calc();
+        assertEquals(3, result);
     }
 
+    @Test
+    @DisplayName("- 계산하기")
+    void calcMinus() {
+        StringCalculator stringCalculator = new StringCalculator("4-2");
+        int result = stringCalculator.calc();
+        assertEquals(2, result);
+    }
+
+    @Test
+    @DisplayName("* 계산하기")
+    void calcMulti() {
+        StringCalculator stringCalculator = new StringCalculator("1*2");
+        int result = stringCalculator.calc();
+        assertEquals(2, result);
+    }
+
+    @Test
+    @DisplayName("/ 계산하기")
+    void calcDivision() {
+        StringCalculator stringCalculator = new StringCalculator("4/2");
+        int result = stringCalculator.calc();
+        assertEquals(2, result);
+    }
 }
