e3e81c69ec89095206c9e7b028bddb97124d110d
lkimilhol <lkimilhol@gmail.com>
2021-04-07 02:58:26 +0900

    enum 수정하여 내부에 람다식을 이용하여 계산 되도록 수정

--- lkimilhol/src/main/java/stringCalculator/Operator.java
+++ lkimilhol/src/main/java/stringCalculator/Operator.java
@@ -1,15 +1,30 @@
 package stringCalculator;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 public enum Operator {
-    PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/"),;
+    PLUS("+", (a, b) -> a + b)
+    , MINUS("-", (a, b) -> a - b)
+    , TIMES("*", (a, b) -> a * b)
+    , DIVIDE("/", (a, b) -> a / b)
+    ,;
 
     String operator;
+    BiFunction<Integer, Integer, Integer> expression;
 
-    Operator(String s) {
-        this.operator = s;
+    Operator(String operator, BiFunction<Integer, Integer, Integer> expression) {
+        this.operator = operator;
+        this.expression = expression;
     }
 
-    public String getOperator() {
-        return operator;
+    public static int calculate(String operator, int num1, int num2) {
+        return getOperator(operator).expression.apply(num1, num2);
+    }
+
+    private static Operator getOperator(String operator) {
+        return Arrays.stream(values())
+                .filter(o -> o.operator.equals(operator))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("올바른 연산자가 아닙니다."));
     }
 }
