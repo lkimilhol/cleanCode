c6e6aa234b5072c8f16d1a854460964ae977aa97
lkimilhol <lkimilhol@gmail.com>
2021-04-07 03:19:14 +0900

    계산기에서 String[] 로의 반환을 큐를 이용하여 리턴하도록 수정.

--- lkimilhol/src/main/java/stringCalculator/StringCalculator.java
+++ lkimilhol/src/main/java/stringCalculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package stringCalculator;
 
+import java.util.*;
+
 public class StringCalculator {
     String stringCalculation;
 
@@ -13,7 +15,7 @@ public class StringCalculator {
 
     public int calc() {
         String operator = injectOperator();
-        String[] numbers = injectNumbers();
+        Queue<Integer> numbers = injectNumbers();
 
         checkNumberCount(numbers);
         return calculate(operator, numbers);
@@ -23,21 +25,21 @@ public class StringCalculator {
         return this.getStringCalculation().replaceAll("[0-9]", "");
     }
 
-    public String[] injectNumbers() {
-        return this.getStringCalculation().split("[+/*-]");
+    public Queue<Integer> injectNumbers() {
+        String[] arr = this.getStringCalculation().split("[+/*-]");
+        Queue<Integer> numbers = new LinkedList<>();
+        numbers.add(Integer.parseInt(arr[0]));
+        numbers.add(Integer.parseInt(arr[1]));
+        return numbers;
     }
 
-    private int parseInt(String s) {
-        return Integer.parseInt(s);
-    }
-
-    private void checkNumberCount(String[] numbers) {
-        if (numbers.length != 2) {
+    private void checkNumberCount(Queue<Integer> numbers) {
+        if (numbers.size() != 2) {
             throw new IllegalArgumentException();
         }
     }
 
-    private int calculate(String operator, String[] numbers) {
-        return Operator.calculate(operator, parseInt(numbers[0]), parseInt(numbers[1]));
+    private int calculate(String operator, Queue<Integer> numbers) {
+        return Operator.calculate(operator, numbers.poll(), numbers.poll());
     }
 }
