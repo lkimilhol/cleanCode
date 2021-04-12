bbaaa1bc614e77213bfc263076c9d711454ed729
lkimilhol <lkimilhol@gmail.com>
2021-04-12 22:27:59 +0900

    getter 추가

--- lkimilhol/src/main/java/lunchEvent/Member.java
+++ lkimilhol/src/main/java/lunchEvent/Member.java
@@ -3,4 +3,17 @@ package lunchEvent;
 public class Member {
     String name;
     int groupNumber;
+
+    public Member(String name, int groupNumber) {
+        this.name = name;
+        this.groupNumber = groupNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGroupNumber() {
+        return groupNumber;
+    }
 }
