4965833c704c909bdd607836d70c09ef33edecc5
lkimilhol <lkimilhol@gmail.com>
2021-04-12 22:28:28 +0900

    랜덤으로 뽑은 회원 리스트의 수가 의도한 수 인지 체크하는 테스트케이스 추가

---
+++ lkimilhol/src/test/java/lunchEvent/LunchEventTest.java
@@ -0,0 +1,20 @@
+package lunchEvent;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LunchEventTest {
+    @Test
+    @DisplayName("랜덤 추출한 회원의 수가 의도한 수인지 체크")
+    void memberListCnt() {
+        final int groupSize = 20;
+        LunchEvent lunchEvent = new LunchEvent(groupSize);
+        List<Member> group = lunchEvent.choiceLunchGroup();
+        Assertions.assertEquals(groupSize, group.size());
+    }
+}
