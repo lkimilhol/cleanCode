5773366b4ebebc739f8f608e17dbc990a8751cd0
lkimilhol <lkimilhol@gmail.com>
2021-04-12 22:40:24 +0900

    - 이심점심 리스트 클래스 생성 - 생성자로 받은 멤버의 수를 기준으로 랜덤하게 사람을 뽑는 로직 개발

---
+++ lkimilhol/src/main/java/lunchEvent/LunchEvent.java
@@ -0,0 +1,41 @@
+package lunchEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LunchEvent {
+    private final int memberCnt;
+    private final List<Member> memberList;
+
+    public LunchEvent(int memberCnt) {
+        this.memberCnt = memberCnt;
+        memberList = new ArrayList<>();
+    }
+
+    public List<Member> choiceLunchGroup() {
+        while (memberList.size() < this.memberCnt) {
+            int idx = getRandomIndex();
+            Member member = new Member(getMemberName(idx), getGroupNumber());
+            memberList.add(member);
+            removePeopleIndex(idx);
+        }
+        return this.memberList;
+    }
+
+    private int getRandomIndex() {
+        return new Random().nextInt(Constant.PEOPLE_LIST.size());
+    }
+
+    private int getGroupNumber() {
+        return this.memberList.size() / Constant.GROUP_CNT + 1;
+    }
+
+    private String getMemberName(int idx) {
+        return Constant.PEOPLE_LIST.get(idx);
+    }
+
+    private void removePeopleIndex(int idx) {
+        Constant.PEOPLE_LIST.remove(idx);
+    }
+}
