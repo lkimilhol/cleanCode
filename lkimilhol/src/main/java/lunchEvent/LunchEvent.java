0c5eed38786dcef434c8b941517c5fb2c774f548
lkimilhol <lkimilhol@gmail.com>
2021-04-14 08:04:01 +0900

    이심점심 개발 완료 -커밋 단위를 쪼개지 못했다-

--- lkimilhol/src/main/java/lunchEvent/LunchEvent.java
+++ lkimilhol/src/main/java/lunchEvent/LunchEvent.java
@@ -5,8 +5,9 @@ import java.util.List;
 import java.util.Random;
 
 public class LunchEvent {
-    private final int memberCnt;
+    private int memberCnt;
     private final List<Member> memberList;
+    private int groupCnt;
     private Member botanist;
 
     public LunchEvent(int memberCnt) {
@@ -15,13 +16,36 @@ public class LunchEvent {
     }
 
     public List<Member> choiceLunchGroup() {
+        if (hasBotanist()) {
+            this.botanist = selectMember();
+            this.memberCnt--;
+        }
+
         while (memberList.size() < this.memberCnt) {
-            int idx = getRandomIndex();
-            Member member = new Member(getMemberName(idx), getGroupNumber());
-            memberList.add(member);
-            removePeopleIndex(idx);
+            memberList.add(selectMember());
         }
         return this.memberList;
+    }
+
+    public void printLunchGroup() {
+        System.out.print("식물 담당자: ");
+        System.out.println(this.botanist == null ? "없음" : this.botanist.getName());
+        System.out.println();
+        for (int i = 1; i <= this.groupCnt; i++) {
+            System.out.println("[" + i + "조]");
+            printLunchMember(i);
+        }
+    }
+
+    private void printLunchMember(int i) {
+        this.memberList.stream().filter(member -> member.getGroupNumber() == i).forEach(member -> System.out.println(member.getName()));
+    }
+
+    protected Member selectMember() {
+        int idx = getRandomIndex();
+        Member member = new Member(getMemberName(idx), getGroupNumber());
+        removePeopleIndex(idx);
+        return member;
     }
 
     private int getRandomIndex() {
@@ -29,11 +53,23 @@ public class LunchEvent {
     }
 
     private int getGroupNumber() {
-        return this.memberList.size() / Constant.GROUP_CNT + 1;
+        int currentGroupCnt = getCurrentGroupCnt();
+        increaseGroupCnt(currentGroupCnt);
+        return currentGroupCnt;
     }
 
-    protected boolean isBotanist() {
-        return this.memberCnt % Constant.GROUP_CNT == 0;
+    protected int getCurrentGroupCnt() {
+        return (this.memberList.size() / Constant.GROUP_CNT) + 1;
+    }
+
+    private void increaseGroupCnt(int currentGroupCnt) {
+        if (this.groupCnt < currentGroupCnt) {
+            this.groupCnt = currentGroupCnt;
+        }
+    }
+
+    protected boolean hasBotanist() {
+        return this.memberCnt % Constant.GROUP_CNT != 0 && this.memberCnt > 1;
     }
 
     private String getMemberName(int idx) {
