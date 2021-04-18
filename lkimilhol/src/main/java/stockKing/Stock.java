766158b9a7052a5916b16406aff09a449e5e6035
lkimilhol <lkimilhol@gmail.com>
2021-04-18 15:03:35 +0900

    차이를 구할 리스트까지 생성 -> 리팩토링 예정

--- lkimilhol/src/main/java/stockKing/Stock.java
+++ lkimilhol/src/main/java/stockKing/Stock.java
@@ -6,25 +6,30 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Stock {
-    final private List<Integer> priceList;
-
-    private List<Integer> prevPriceList;
+    final private List<Integer> currentPriceList;
+    final private List<Integer> prevPriceList;
+    final private List<Integer> differentPriceList;
 
     public Stock() {
-        this.priceList = makeStockPrice();
+        this.currentPriceList = makeStockPrice();
+        this.prevPriceList = makeStockPrice();
+        this.differentPriceList = new ArrayList<>();
+        calculateDifferentPriceList();
     }
 
-    public List<Integer> getPriceList() {
-        return priceList;
+    private void calculateDifferentPriceList() {
+        for (int i = 0; i < Constant.STOCK_MAX_COUNT; i++) {
+            differentPriceList.add(currentPriceList.get(i) - prevPriceList.get(i));
+        }
     }
 
-    public List<Integer> getPrevPriceList() {
-        return prevPriceList;
+    public List<Integer> getCurrentPriceList() {
+        return currentPriceList;
     }
 
     protected List<Integer> makeStockPrice() {
         Random random = new Random();
-         return IntStream.generate(() -> random.nextInt(Constant.STOCK_MAX_PRICE) + 1)
+        return IntStream.generate(() -> random.nextInt(Constant.STOCK_MAX_PRICE) + 1)
                 .limit(Constant.STOCK_MAX_COUNT)
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
     }
