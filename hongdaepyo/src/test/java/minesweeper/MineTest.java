6167f3ecc489496e1355c88addd58a5921893a55
Hong Daepyo <h1o1n1g1@nate.com>
2021-05-02 01:40:14 +0900

    MineTest에 상수 치환

--- hongdaepyo/src/test/java/minesweeper/MineTest.java
+++ hongdaepyo/src/test/java/minesweeper/MineTest.java
@@ -19,7 +19,7 @@ class MineTest {
         Mine mine = new Mine();
 
         Assertions.assertTrue(mine.getRow() >= 0);
-        Assertions.assertTrue(mine.getRow() < 8);
+        Assertions.assertTrue(mine.getRow() < Constant.DEFAULT_HEIGHT);
     }
 
     @Test
@@ -27,7 +27,7 @@ class MineTest {
         Mine mine = new Mine();
 
         Assertions.assertTrue(mine.getCol() >= 0);
-        Assertions.assertTrue(mine.getCol() < 10);
+        Assertions.assertTrue(mine.getCol() < Constant.DEFAULT_WIDTH);
     }
 
     @Test
